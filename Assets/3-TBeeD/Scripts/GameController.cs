1bd1915d81e3d7dd26513184554bcdead85399f5
Tony Nguyen <blue38275@yahoo.com>
2020-12-28 16:37:24 -0600

    test flip bread again

--- Assets/3-TBeeD/Scripts/GameController.cs
+++ Assets/3-TBeeD/Scripts/GameController.cs
@@ -4,14 +4,5 @@ namespace TBeeD
 {
     public class GameController : MonoBehaviour
     {
-        [SerializeField] private Bread rightBread;
-
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.I))
-            {
-                rightBread.Flip();
-            }
-        }
     }
 }
---
+++ Assets/3-TBeeD/Scripts/Plate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TBeeD
+{
+    public class Plate : MonoBehaviour
+    {
+        [SerializeField] private Bread rightBread;
+        [SerializeField] private float duration = 0f;
+        [SerializeField] private float moveSpeed = 0f;
+        [SerializeField] private float enterPositionX = 0f;
+        [SerializeField] private float exitPositionX = 0f;
+        private float timer;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                rightBread.Flip();
+            }
+        }
+
+        IEnumerator OnEnter()
+        {
+            yield return null;
+        }
+
+        IEnumerator OnExit()
+        {
+            while (transform.position.x > exitPositionX)
+            {
+                transform.Translate(Vector2.left * moveSpeed);
+            }
+
+            transform.position = new Vector2(enterPositionX, transform.position.y);
+            yield return null;
+        }
+    }
+}
