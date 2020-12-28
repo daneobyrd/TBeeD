337f6b853cff6c5428633b51b572b9722d90d8d3
Tony Nguyen <blue38275@yahoo.com>
2020-12-28 00:07:12 -0600

    Added dash and normalized moveinput

--- Assets/3-TBeeD/Scripts/BeeController.cs
+++ Assets/3-TBeeD/Scripts/BeeController.cs
@@ -4,8 +4,11 @@ namespace TBeeD
 {
     public class BeeController : MonoBehaviour
     {
-        [SerializeField] private float currentSpeed = 0f;
+        [SerializeField] private float moveSpeed = 0f;
+        [SerializeField] private float dashForce = 0f;
         private Rigidbody2D rb;
+        private Vector2 moveInput;
+        private bool activatedDash;
 
         void Awake()
         {
@@ -14,17 +17,44 @@ namespace TBeeD
 
         void Update()
         {
+            HandleInput();
+        }
+
+        void FixedUpdate()
+        {
+            rb.velocity = moveInput * moveSpeed;
+            transform.up = rb.velocity.normalized;
+
+            if (activatedDash)
+            {
+                Dash();
+            }
+        }
+
+        void HandleInput()
+        {
             Move();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                activatedDash = true;
+            }
         }
 
         void Move()
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
+            moveInput = new Vector2(horizontal, vertical).normalized;
+        }
 
-            Vector2 moveInput = new Vector2(horizontal, vertical);
-            rb.velocity = moveInput * currentSpeed;
-            transform.up = rb.velocity.normalized;
+        void Dash()
+        {
+            if (activatedDash)
+            {
+                rb.AddForce(transform.up * dashForce, ForceMode2D.Impulse);
+                activatedDash = false;
+            }
         }
     }
 }
