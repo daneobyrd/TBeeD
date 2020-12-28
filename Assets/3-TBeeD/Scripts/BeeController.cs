66691b6abe73127ae1ffbb3375f968196d4b458d
Tony Nguyen <blue38275@yahoo.com>
2020-12-27 23:45:47 -0600

    Added basic move input
    -bee faces direction of movement

---
+++ Assets/3-TBeeD/Scripts/BeeController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace TBeeD
+{
+    public class BeeController : MonoBehaviour
+    {
+        [SerializeField] private float currentSpeed = 0f;
+        private Rigidbody2D rb;
+
+        void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector2 moveInput = new Vector2(horizontal, vertical);
+            rb.velocity = moveInput * currentSpeed;
+            transform.up = rb.velocity.normalized;
+        }
+    }
+}
