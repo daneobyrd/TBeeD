528725b466c23239a5d28c90b9f2bb0c77db3ab8
Tony Nguyen <blue38275@yahoo.com>
2020-12-28 15:12:25 -0600

    adding test bread flip

---
+++ Assets/3-TBeeD/Scripts/Bread.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TBeeD
+{
+    public class Bread : MonoBehaviour
+    {
+        private Animator animator;
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        internal void Flip()
+        {
+            animator.SetTrigger("Flip");
+        }
+    }
+}
--- Assets/3-TBeeD/Scripts/GameController.cs
+++ Assets/3-TBeeD/Scripts/GameController.cs
@@ -4,5 +4,14 @@ namespace TBeeD
 {
     public class GameController : MonoBehaviour
     {
+        [SerializeField] private Bread rightBread;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                rightBread.Flip();
+            }
+        }
     }
 }
