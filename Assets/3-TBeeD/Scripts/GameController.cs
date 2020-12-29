34598855f173e099e9fd5e01ba2f092c5cc1faa2
Amber Smith <amberrociosmith@gmail.com>
2020-12-28 20:25:52 -0600

    added buzzing and implementation for win/lose in gameController

--- Assets/3-TBeeD/Scripts/GameController.cs
+++ Assets/3-TBeeD/Scripts/GameController.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace TBeeD
 {
     public class GameController : MonoBehaviour
     {
+        [SerializeField] private UnityEvent onStart;
+        [SerializeField] private UnityEvent onWin;
+        [SerializeField] private UnityEvent onLose;
+
+        private void Start()
+        {
+            onStart.Invoke();
+        }
+        public void OnWin()
+        {
+            onWin.Invoke();
+        }
+        public void OnLose()
+        {
+            onLose.Invoke();
+        }
     }
 }
